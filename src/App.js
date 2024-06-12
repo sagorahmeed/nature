c131802b9fbe0a066c97fff6b75799d04ff2c8f3
sagorahmeed <sagorahmeed6060@gmail.com>
2024-06-12 23:51:17 +0600

    updated home page

--- src/App.js
+++ src/App.js
@@ -154,6 +154,12 @@ import Brand from './pages/Brand';
 import ScrollToTop from './components/ScrollToTop';
 import Result from './pages/Result';
 import Error from './pages/404';
+import { pdfjs } from 'react-pdf';
+
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 const App = () => {
   return (
--- src/components/PDFModal.js
+++ src/components/PDFModal.js
@@ -1,8 +1,30 @@
+// import React from 'react';
+// import Modal from 'react-modal';
+
+// const PDFModal = ({ isOpen, onRequestClose, pdfUrl }) => {
+//     console.log('pdfUrl',pdfUrl)
+//     return (
+//         <Modal
+//             isOpen={isOpen}
+//             onRequestClose={onRequestClose}
+//             contentLabel="PDF Modal"
+//             className="modal"
+//             overlayClassName="overlay"
+//         >
+//             <button onClick={onRequestClose}>x</button>
+//             <embed src={pdfUrl} type="application/pdf" width="100%" height="100%" />
+//         </Modal>
+//     );
+// };
+
+// export default PDFModal;
+
+
 import React from 'react';
 import Modal from 'react-modal';
+import MyPdfViewer from './MyPdfViewer';
 
 const PDFModal = ({ isOpen, onRequestClose, pdfUrl }) => {
-    console.log('pdfUrl',pdfUrl)
     return (
         <Modal
             isOpen={isOpen}
@@ -11,8 +33,8 @@ const PDFModal = ({ isOpen, onRequestClose, pdfUrl }) => {
             className="modal"
             overlayClassName="overlay"
         >
-            <button onClick={onRequestClose}>x</button>
-            <embed src={pdfUrl} type="application/pdf" width="100%" height="100%" />
+            <button onClick={onRequestClose}>Close</button>
+            <MyPdfViewer pdfUrl={pdfUrl} />
         </Modal>
     );
 };
